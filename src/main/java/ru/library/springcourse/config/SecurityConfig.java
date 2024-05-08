306c78fc367fd45aa3a51758ef96ec168f605923
sergei_darin <swd86@mail.ru>
2024-05-08 20:38:24 +0400

    Добавлена кастомная форма для ввода логина и пароля

--- src/main/java/ru/library/springcourse/config/SecurityConfig.java
+++ src/main/java/ru/library/springcourse/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package ru.library.springcourse.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -24,8 +25,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
         this.personDetailsService = personDetailsService;
     }
 
+    // настраиваем сам Spring Security, то какую форму для логина использовать
+    // конфигурируем авторизацию
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/auth/login","/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/library/books",true)
+                .failureUrl("/auth/login?error");
+    }
+
     //настраивает аутентификацию
     //даем понять Spring Security, что мы используем провайдер для аутентификации пользователя
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService);
     }
