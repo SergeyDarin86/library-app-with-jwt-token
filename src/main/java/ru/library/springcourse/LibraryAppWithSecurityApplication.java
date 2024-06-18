05ed8cbced60a29e49bf10346364ab0bf595f053
sergei_darin <swd86@mail.ru>
2024-06-18 22:50:14 +0400

    1) Добавил зависимости для маппера + jwt-токена 2) Создал заготовки для PersonDTO и BookDTO

--- src/main/java/ru/library/springcourse/LibraryAppWithSecurityApplication.java
+++ src/main/java/ru/library/springcourse/LibraryAppWithSecurityApplication.java
@@ -1,13 +1,20 @@
 package ru.library.springcourse;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LibraryAppWithSecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryAppWithSecurityApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
 	}
 
 }
---
+++ src/main/java/ru/library/springcourse/dto/BookDTO.java
@@ -0,0 +1,5 @@
+package ru.library.springcourse.dto;
+
+public class BookDTO {
+
+}
