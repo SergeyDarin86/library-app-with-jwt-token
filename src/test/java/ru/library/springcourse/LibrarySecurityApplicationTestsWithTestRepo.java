9e7dea39596f6a7641cd2cf5be2af85cdd961dff
sergei_darin <swd86@mail.ru>
2024-07-23 01:10:45 +0400

    Работа с тестовым репозиторием (БД) + создание скриптов для тестовой БД в классе LibrarySecurityApplicationTestsWithTestRepo

---
+++ src/test/java/ru/library/springcourse/LibrarySecurityApplicationTestsWithTestRepo.java
@@ -0,0 +1,119 @@
+package ru.library.springcourse;
+
+import org.hamcrest.Matchers;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import ru.library.springcourse.controllers.BookController;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+// говорит, что нам нужно выполнить sql-скрипт перед выполнением теста
+// может быть указана на уровне класса или на уровне любого метода
+
+@Sql(value = {"/create-person-before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = {"/delete-person-after.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource("/application-test.properties") // указываем файл с пропертями для тестового окружения
+public class LibrarySecurityApplicationTestsWithTestRepo {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private BookController bookController;
+
+    String token;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        ResultActions resultActions = this.mockMvc
+                .perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content("{\"login\":\"user\",\"password\":\"user\"}")
+                        .accept(MediaType.APPLICATION_JSON_VALUE));
+
+        MvcResult mvcResult = resultActions.andDo(print()).andReturn();
+        String contentAsString = mvcResult.getResponse().getContentAsString();
+
+        JSONObject jsonObject = new JSONObject(contentAsString);
+        this.token = "Bearer " + jsonObject.getString("jwt-token");
+
+    }
+
+    @Test
+    void contextLoads() {
+        assertThat(bookController).isNotNull();
+    }
+
+    @Test
+    void adminPage() throws Exception {
+        this.mockMvc.perform(get("/library/admin")
+                        .accept(MediaType.APPLICATION_JSON).header("Authorization", this.token))
+                .andExpect(content().string(containsString("/people/adminPage")))
+                .andExpect(status().is2xxSuccessful())
+                .andDo(print());
+    }
+
+    @Test
+    void getAllPeople() throws Exception {
+        this.mockMvc
+                .perform(get("/library/people").accept(MediaType.APPLICATION_JSON)
+                        .header("Authorization", this.token))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("personDTOList", Matchers.hasSize(2)));
+    }
+
+    @Test
+    void getAllBooks() throws Exception {
+        this.mockMvc
+                .perform(get("/library/books").accept(MediaType.APPLICATION_JSON)
+                        .header("Authorization", this.token))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("bookDTOList", Matchers.hasSize(2)));
+    }
+
+    @Test
+    void showBookById() throws Exception {
+        int id = 1;
+        this.mockMvc.perform(get("/library/books/{id}", id)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .header("Authorization", this.token))
+                .andExpect(status().isOk());
+//        verify(bookController,times(1)).showBook(id);
+    }
+
+    @Test
+    void showBookWithThrowException() throws Exception {
+        int id = 12;
+        this.mockMvc.perform(get("/library/books/{id}", id)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .header("Authorization", this.token))
+                .andExpect(status().isNotFound());
+//        verify(bookController,times(1)).showBook(id);
+    }
+
+    @Test
+    void showPersonById() throws Exception {
+        int personId = 1;
+        this.mockMvc.perform(get("/library/people/{id}", personId)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .header("Authorization", this.token))
+                .andExpect(status().isOk());
+    }
+
+}
