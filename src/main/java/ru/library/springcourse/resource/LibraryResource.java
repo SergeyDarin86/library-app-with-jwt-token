d90e537505a010179c1dd5113a8b18c3544c8957
sergei_darin <swd86@mail.ru>
2024-08-04 00:33:14 +0400

    Продолжение работы с документацией для LibraryResource

--- src/main/java/ru/library/springcourse/resource/LibraryResource.java
+++ src/main/java/ru/library/springcourse/resource/LibraryResource.java
@@ -1,5 +1,6 @@
 package ru.library.springcourse.resource;
 
+import io.swagger.annotations.ApiParam;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
@@ -30,7 +31,10 @@ public interface LibraryResource {
     @GetMapping("/admin")
     String adminPage();
 
-    @Operation(description = "Отправка запроса на получение всех читателей")
+    @Operation(
+            summary = "Список всех читателей",
+            description = "Отправка запроса на получение всех читателей"
+    )
     @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "200",
@@ -50,7 +54,13 @@ public interface LibraryResource {
     @GetMapping("/people")
     PersonResponse people();
 
-    @Operation(description = "Отправка запроса на получение всех книг в библиотеке")
+    @Operation(
+            summary = "Список всех книг",
+            description = "Отправка запроса на получение всех книг в библиотеке: " +
+            "\n - с возможностью сортировки по году издания;" +
+            "\n - с возможностью выбора номера страницы;" +
+            "\n - с возможностью выбора количества книг на странице."
+    )
     @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "200",
@@ -68,34 +78,75 @@ public interface LibraryResource {
                     })
     })
     @GetMapping("/books")
-    BookResponse books(@RequestParam(value = "isSortedByYear", required = false) Boolean isSortedByYear,
-                              @RequestParam(value = "page", required = false) Integer page,
-                              @RequestParam(value = "limitOfBooks", required = false) Integer limitOfBooks);
+    BookResponse books(@ApiParam(value = "Возможность сортировки книг по году издания") @RequestParam(value = "isSortedByYear", required = false) Boolean isSortedByYear,
+                       @ApiParam(value = "Номер выводимой страницы") @RequestParam(value = "page", required = false) Integer page,
+                       @ApiParam(value = "Количество книг на странице") @RequestParam(value = "limitOfBooks", required = false) Integer limitOfBooks);
 
+    @Operation(
+            summary = "Вывод отсортированного списка страниц. " +
+            "\n(Использовал отдельную кнопку при работе с Thymeleaf)"
+    )
     @GetMapping("/sortedByYear")
-    public BookResponse sortedBooksByYear();
+    BookResponse sortedBooksByYear();
 
+    @Operation(
+            summary = "Добавление новой книги",
+            description = "Необходимо ввести корректные данне новой книги," +
+            " которые соответствуют параметрам." +
+            "\n Смотри ограничения во вкладке \"Model\""
+    )
     @PostMapping("/newBook")
     ResponseEntity newBook(@RequestBody @Valid BookDTO bookDTO, BindingResult bindingResult);
 
-    @Operation(description = "Отправка запроса на получение книги по её идентификатору")
+    @Operation(
+            summary = "Получение книги по ее идентификационному номеру",
+            description = "Необходимо отправить корректный идентификатор книги."
+    )
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
             @ApiResponse(responseCode = "404", description = "Not found - Книги с таким id не найдено")
     })
     @GetMapping("/books/{id}")
     @ResponseBody
-    ResponseEntity<BookDTO> showBook( @Parameter(name = "id", description = "Идентификатор книги для запроса на её получение", example = "1")@PathVariable("id") int id);
+    ResponseEntity<BookDTO> showBook(
+            @Parameter(name = "id", description = "Идентификатор книги для запроса на её получение", example = "1") @PathVariable("id") int id
+    );
 
+    @Operation(
+            summary = "Редактирование данных книги",
+            description = "Необходимо отправить корректный идентификатор книги."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "404", description = "Not found - Книги с таким id не найдено")
+    })
     @PatchMapping("/books/{id}")
     ResponseEntity updateBook(@RequestBody @Valid BookDTO bookDTO, BindingResult bindingResult,
-                                     @PathVariable("id") int id);
+                              @ApiParam(value = "Идентификатор книги", example = "3") @PathVariable("id") int id);
 
+    @Operation(
+            summary = "Освободить книгу",
+            description = "Необходимо отправить корректный идентификатор книги."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "404", description = "Not found - Книги с таким id не найдено")
+    })
     @PatchMapping("/books/{id}/makeFree")
-    ResponseEntity makeBookFree(@PathVariable("id") int id);
+    ResponseEntity makeBookFree(@ApiParam(value = "Идентификатор книги", example = "3") @PathVariable("id") int id);
 
+    @Operation(
+            summary = "Закрепить книгу за читателем",
+            description = "Необходимо отправить корректные идентификаторы книги и читателя."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "404", description = "Not found - Книги с таким id не найдено/ Читателя с таким id не найдено")
+    })
     @PatchMapping("/books/{bookId}/{personId}/assignPerson")
-    ResponseEntity assignPerson(@PathVariable("bookId") int bookId, @PathVariable("personId") int personId);
+    ResponseEntity assignPerson(
+            @ApiParam(value = "Идентификатор книги", example = "1") @PathVariable("bookId") int bookId,
+            @ApiParam(value = "Идентификатор читателя", example = "2") @PathVariable("personId") int personId);
 
     @DeleteMapping("/books/{id}")
     ResponseEntity deleteBook(@PathVariable("id") int id);
@@ -103,12 +154,28 @@ public interface LibraryResource {
     @GetMapping("/books/search")
     ResponseEntity search(@RequestParam(value = "searchBook", required = false, defaultValue = "") String searchBook);
 
+    @Operation(
+            summary = "Получение читателя по его идентификационному номеру",
+            description = "Необходимо отправить корректный идентификатор читателя."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "404", description = "Not found - Читателя с таким id не найдено")
+    })
     @GetMapping("/people/{id}")
     ResponseEntity show(@PathVariable("id") int id);
 
+    @Operation(
+            summary = "Редактирование данных читателя",
+            description = "Необходимо отправить корректный идентификатор читателя."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "404", description = "Not found - Читателя с таким id не найдено")
+    })
     @PatchMapping("/people/{id}")
     ResponseEntity updatePerson(@RequestBody @Valid PersonDTO personDTO, BindingResult bindingResult,
-                                       @PathVariable("id") int id);
+                                @PathVariable("id") int id);
 
     @DeleteMapping("/people/{id}")
     ResponseEntity deletePerson(@PathVariable("id") int id);
